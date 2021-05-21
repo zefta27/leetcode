ea4965c5d89a68cc80c8038a9d3fad6cf90db423
ZEFTA ADETYA <zeftaadety@gmail.com>
2021-05-21 09:56:06 +0700

    success

--- 58_length_of_last_word.php
+++ 58_length_of_last_word.php
@@ -1,40 +1,42 @@
 <?
-class Solution
-{
+class Solution {
 
   /**
    * @param String $s
    * @return Integer
    */
-  function lengthOfLastWord($s)
-  {
-
-
-
-    $exp = explode(" ", $s);
-    if (count($exp) > 0) {
-      $max = 0;
-      for ($i = 0; $i < count($exp); $i++) {
-        if ($exp[$i] != "") {
-          $str_split = str_split($exp[$i]);
-          $count_str = count($str_split);
-          if ($count_str > $max) {
-            $max = $count_str;
-          }
-          if ($i == count($exp) - 1) {
-            $max = $count_str;
-          }
-        }
+  function lengthOfLastWord($s) {
+      
+  
+      
+      $exp = explode(" ", $s);
+      if(count($exp)>0)
+      {
+              $res = 0;
+              for($i=count($exp)-1;$i>=0;$i--)
+              {
+                  if($exp[$i]!="")
+                  {
+                      $str_split = str_split($exp[$i]);
+                      $count_str = count($str_split);
+                      $res  = $count_str;
+                      break;
+                  }
+              }
+              return $res;
+       
       }
-      return $max;
-    } else {
-      if ($s != " ") {
-        $str = str_split($exp[0]);
-        $count_str = count($str);
-        return $count_str;
-      } else {
-        return 0;
+      else{
+          if($s!=" ")
+          {
+               $str = str_split($exp[0]);
+               $count_str = count($str);
+              return $count_str;
+          }else{
+              return 0;
+          }
+          
+          
       }
-    }
   }
 }
